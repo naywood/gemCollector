c5c2ef05bb6db81db910594f89fd1121ba59a06f
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-01-12 20:14:59 -0800

    start of JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+//GLOBAL VARIABLES
+//
+
+// Crystal Variables
+var crystal = {
+    bismuth:
+    {
+        name: "Bismuth",
+        value: 0
+    },   
+    rose:
+    {
+        name: "Rose",
+        value: 0
+    },   
+    opal:
+    {
+        name: "Opal",
+        value: 0
+    },  
+    jade:
+    {
+        name: "Jade",
+        value: 0
+    }   
+}
+
+// Score variables
+
+var currentScore = 0;
+var targetScore = 0;
+
+//Wins and Losses variables
+var winCount = 0;
+var lossCount = 0;
+
+//ALL THE FUNCTIONS
+//
+
+
+//MAIN WHERE ALL THE THINGS GET CALLED
+//       
+
