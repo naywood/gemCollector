ee360429790d3da70ce5eab534188f966c20c8ef
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-01-12 21:52:33 -0800

    game is complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,8 +36,85 @@ var lossCount = 0;
 
 //ALL THE FUNCTIONS
 //
+var getRandom = function (min, max) {
+    return Math.floor(Math.random() * ( max - min +1)) +min;
+}
 
+var startGame = function() {
+    
+    // Reset the current score
+    currentScore = 0;
+    
+    // Set new Target Score (19-120)
+    targetScore = getRandom (19, 120);
 
-//MAIN WHERE ALL THE THINGS GET CALLED
+    // different values for each crystal between 1-12
+    crystal.bismuth.value = getRandom (1, 12);
+    crystal.rose.value = getRandom (1, 12);
+    crystal.opal.value =  getRandom (1, 12);
+    crystal.jade.value = getRandom (1, 12);
+
+    // Change HTML to reflect all changes
+    $("#yourScore").html(currentScore);
+    $("#targetScore").html(targetScore);
+
+    //restart the game
+}
+
+// FUNCTION THAT RESPONDS TO CLICKS
+
+var addValues = function(crystal) {
+currentScore = currentScore + crystal.value; 
+//adding to card
+$("#yourScore").html(currentScore);
+checkWin();
+}
+
+//check if user won or lost and resets
+var checkWin = function(){
+
+    //check if current score is equal to target score or not
+    if (currentScore > targetScore) {
+        alert ("YOU LOST!");
+        //adds to loss counter
+        lossCount++;
+        //change loss counter in html
+        $("#lost").html(lossCount);
+        //restart the game
+        startGame();
+
+    }
+    else if (currentScore == targetScore){
+        alert("YOU WONNNNNNN!"); 
+        //adds to win counter
+        winCount++;
+        //change win count in HTML
+        $("win").html(winCount);
+        //restart the game
+        startGame();
+
+    }
+
+}
+
+//MAIN WHERE ALL THE THINGS GET CALLED and things function
 //       
 
+//starts the game for the first time
+startGame();
+
+
+$("#bismuth").click(function() {
+    addValues(crystal.bismuth);
+});
+
+$("#rose").click(function() {
+    addValues(crystal.rose);
+});
+$("#opal").click(function() {
+    addValues(crystal.opal);
+});
+
+$("#jade").click(function() {
+    addValues(crystal.jade);
+});
